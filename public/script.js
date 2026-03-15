eb4942fae03f9f5da86943831e4670de31ae8b5c
Vlad <noipserverm@gmail.com>
2026-03-15 14:42:07 +0200

    feat: initial layout of category pages

--- public/script.js
+++ public/script.js
@@ -1,4 +1,7 @@
-const container = document.getElementById("special-products-container");
+const specialContainer = document.getElementById("special-products-container");
+const categoryContainer = document.getElementById(
+  "category-products-container",
+);
 
 //fetch("http://localhost:3000/products")
 fetch("/products/special")
@@ -13,7 +16,66 @@ fetch("/products/special")
         <p>Ціна: ${product.price} грн</p>
         <button data-id="${product.id}">Додати в корзину</button>
       `;
-      container.appendChild(card);
+      specialContainer.appendChild(card);
     });
   })
   .catch((err) => console.error("Error loading products", err));
+
+const params = new URLSearchParams(window.location.search);
+const categoryId = params.get("categoryId");
+
+fetch(`/products/category/${categoryId}`)
+  .then((response) => response.json())
+  .then((products) => {
+    products.forEach((product) => {
+      const card = document.createElement("div");
+      card.classList.add("product-card");
+      card.innerHTML = `
+      <h3>${product.name}</h3>
+        <p>Ціна: ${product.price} грн</p>
+        <button data-id="${product.id}">Додати в корзину</button>
+      `;
+      categoryContainer.appendChild(card);
+    });
+  })
+  .catch((err) => console.error("Error loading products", err));
+
+const categoryImage = document.getElementById("category-image");
+const categoryTitle = document.getElementById("category-title");
+
+fetch(`/categories/${categoryId}`)
+  .then((response) => response.json())
+  .then((category) => {
+    categoryImage.src = `images/${category.image}`;
+    categoryTitle.textContent = category.name;
+  });
+
+const catalogList = document.getElementById("catalog-list");
+
+const catalogContainer = document.getElementById("catalog-container");
+
+fetch("/categories")
+  .then((res) => res.json())
+  .then((categories) => {
+    categories.forEach((category) => {
+      //sidebar
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <a href="category.html?categoryId=${category.id}">
+          ${category.name}
+        </a>
+      `;
+      if (catalogList) catalogList.appendChild(li);
+
+      //grid catalog
+      const card = document.createElement("div");
+      card.classList.add("catalog-item");
+      card.innerHTML = `
+          <img src="images/${category.image}" alt="${category.name}">
+          <a href="category.html?categoryId=${category.id}">
+            ${category.name}
+          </a>
+        `;
+      if (catalogContainer) catalogContainer.appendChild(card);
+    });
+  });
--- server.js
+++ server.js
@@ -24,3 +24,42 @@ app.get("/products/special", (req, res) => {
     res.json(results);
   });
 });
+
+app.get("/products/category/:id", (req, res) => {
+  const categoryId = req.params.id;
+  const query = "SELECT * FROM products WHERE category_id = ?";
+  db.query(query, [categoryId], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Server error" });
+    }
+    res.json(results);
+  });
+});
+
+app.get("/categories/:id", (req, res) => {
+  const categoryId = req.params.id;
+
+  const query = "SELECT * FROM categories WHERE id = ?";
+
+  db.query(query, [categoryId], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Server error" });
+    }
+
+    res.json(results[0]);
+  });
+});
+
+app.get("/categories", (req, res) => {
+  const query = "SELECT * FROM categories";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Server error" });
+    }
+
+    res.json(results);
+  });
+});
